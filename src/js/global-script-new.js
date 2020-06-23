948b01e9c328c91d758746c72a16cff3422f0e67
Nikolay Gromov <nicothin@gmail.com>
2020-06-23 23:10:25 +0300

    Разобрался с поиском: пришлось дублировать :(

--- src/js/global-script-new.js
+++ src/js/global-script-new.js
@@ -1,16 +1,31 @@
 $( document ).ready(function() {
 
-  // Детектор направления скролла
+  // Детектор направления скролла и связанные эффекты
   var lastScrollTop = 0;
   window.addEventListener("scroll", function () {
     var st = window.pageYOffset || document.documentElement.scrollTop;
+    // Посетитель скроллит вниз
     if (st > lastScrollTop) {
-      $('body').addClass('scroll-to-down').removeClass('scroll-to-up');
-    } else {
-      $('body').removeClass('scroll-to-down').addClass('scroll-to-up');
+      // $('body').addClass('scroll-to-down').removeClass('scroll-to-up');
+      // Если скролл уже больше высоты шапки на мобилке, добавим класс сокрытия поиска шапки
+      if(st > 96) {
+        $('body').addClass('hide-header-search-xs');
+      }
+      // Скролл меньше высоты шапки - уберем класс сокрытия поиска
+      else {
+        $('body').removeClass('hide-header-search-xs');
+      }
+    }
+    // Посетитель скроллит вверх
+    else {
+      // $('body').removeClass('scroll-to-down').addClass('scroll-to-up');
+      // Убираем класс сокрытия поиска в шапке
+      $('body').removeClass('hide-header-search-xs');
     }
     lastScrollTop = st <= 0 ? 0 : st;
   }, false);
+
+
 
   // Локация: Переключение «вкладок» фото/карта/улицы/
   $('.choose_show a').on('click', function (e) {
@@ -18,6 +33,8 @@ $( document ).ready(function() {
     $('.sw_show').hide();
     $($(this).attr('href')).show();
   })
+
+
 
   // Метод, определяющий видиомость чего-либо во вьюпорте
   $.fn.showInViewport = function () {
@@ -28,6 +45,8 @@ $( document ).ready(function() {
     return elementBottom > viewportTop && elementTop < viewportBottom;
   };
 
+
+
   // Локация: Показ/сокрытие кнопки добавления в избранное
   $(window).on('resize scroll', function () {
     if ($('#hide-favorite-btn').showInViewport()) {
@@ -36,6 +55,8 @@ $( document ).ready(function() {
       $('#temp-id1').removeClass('b-save-btn--hidden');
     }
   });
+
+
 
   // ВРЕМЕННОЕ ДЕМО! ТОЛКЬО НЕ В ПРОД! Локация: Визуализация работы кнопки Save
   $('#temp-id1').on('click', function () {
